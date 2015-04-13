1d7a037785fb524766d5f27cda288054d0dad618
Longfei Fan <physflf@gmail.com>
2015-04-13 01:10:14 -0500

    shake 100 partially done

--- Shake/main.c
+++ Shake/main.c
@@ -1,7 +1,11 @@
-// Apple ac demagnetization method to find the ground state 
-// 		energy of the Sherrington-Kirkpatrick model.
-// Output the optimal energy, the residue magnetization, and
-// 		the cycle times of each instance.
+/****************************************************************************/
+/*								PHYS-619 Project							*/
+/*	  		Hysteretic optimization for Sherrington-Kirkpatrick model		*/
+/****************************************************************************/
+
+// Apply hysteretic optimation method to find the ground state energy of
+// the Sherrington-Kirkpatrick model. Output the optimal energy, and the
+// residue magnetization.
 
 // v.1.0 -- 04/12/2015 -- Longfei Fan
 
@@ -9,10 +13,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include "r1279.h"
-#include "ACD.h"
+#include "HO.h"
 
 #define INSTANCES 1000
 #define H0 1.6
+#define SHAKES 10
+#define HS 0.7
 
 long seedgen(void);
 
@@ -22,47 +28,44 @@ int main(){
 	seed = seedgen();
 	setr1279(seed);
 
-	FILE *fp1 = fopen("ACD.dat", "w");
-	
-	int i, times;
+	int i, j;
 	GLASS_SK sys;
 	double cycle = 0;
 	double e, e2, m, m2;
 	double e_s = 0, e2_s = 0, m_s = 0, m2_s = 0;
 
-	fprintf(fp1, "# Energy\tEnergy^2\tMagnetic\tMagnet^2\tCycles\n");
+	FILE *fp1 = fopen("shake_10_32.dat", "w");
+	fprintf(fp1, "# Energy\tEnergy^2\tMagnetic\tMagnet^2\n");
+
 	for(i = 0; i < INSTANCES; i++){
-		/* Initialize one instance */
-		sys = init_sys();
-		/* Apply ac demagniztion procedure */
-		times = ac_demag(&sys, H0);
-		// printf("ACD done! Ustable spins: %d\n", sys.unstable_num);
-		/* Output the resutls */
-		e = sys.energy_stable/sys.N;
+		sys = init_sys();		/* Initialize one instance */
+		ac_demag(&sys, H0);		/* Apply ac demagniztion procedure */
+		for(j = 0; j < SHAKES; j++)		/* Shake the systems */	
+			shake(&sys, HS);
+
+		e = sys.energy_stable/sys.N;	/* Output the resutls */
 		e2 = pow(e, 2);
 		m = fabs(sys.magnetization/sys.N);
 		m2 = pow(m, 2);
-		fprintf(fp1, "%.6f\t%.6f\t%.6f\t%.6f\t%d\n", e, e2, m, m2, times);
-		/* Averaging */
-		cycle += times;
-		e_s += e;
+		fprintf(fp1, "%.6f\t%.6f\t%.6f\t%.6f\n", e, e2, m, m2);
+		
+		e_s += e;		/* Summation */
 		e2_s += e2;
 		m_s += m;
 		m2_s += m2;
-		/* Free the space */
-		free_J(&sys);
+		
+		free_J(&sys);	/* Free the space */
 	}
 	fclose(fp1);
 
-	e_s /= INSTANCES;
+	e_s /= INSTANCES;	/* Averaging */
 	e2_s /= INSTANCES;
 	cycle /= INSTANCES;
 	m_s /= INSTANCES;
 	m2_s /= INSTANCES;
 
-	FILE *fp2 = fopen("ACD_ave.dat", "w");
-	fprintf(fp2, "# En_Ave\tEn2_Ave\t\tMag_Ave\t\tMag2_Ave\tCycles_Ave\n");
-	fprintf(fp2, "%.6f\t%.6f\t%.6f\t%.6f\t%f\n", e_s, e2_s, m_s, m2_s, cycle);
+	FILE *fp2 = fopen("shake_32_ave.dat", "at");
+	fprintf(fp2, "%4d\t%.6f\t%.6f\t%.6f\t%.6f\n", SIZE, e_s, e2_s, m_s, m2_s);
 	fclose(fp2);
 
 	return 0;
